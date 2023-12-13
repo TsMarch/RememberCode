ea343cf3b3a92022f6d5f7c931b692c85494b183
TsMarch <dementedcactus@mail.ru>
2023-12-13 21:23:28 +0500

    Mongodb temporary logic change

--- src/main.py
+++ src/main.py
@@ -3,11 +3,25 @@ from fastapi import FastAPI
 from src.auth.base_config import auth_backend, fastapi_users
 from src.auth.schemas import UserRead, UserCreate
 
+from src.python_questions.routes import router as python_router
+from src.python_questions.routes import client
+
 app = FastAPI(title="InterviewApp")
+
+
+@app.on_event("startup")
+def startup_db_client():
+    app.mongodb_client = client
+    app.database = app.mongodb_client["appDB"]
+    series_collection = app.database["series"]
+
 
 app.include_router(fastapi_users.get_auth_router(auth_backend),
                    prefix="/auth/jwt",
                    tags=["Authentication"])
+
 app.include_router(fastapi_users.get_register_router(UserRead, UserCreate),
                    prefix="/register",
                    tags=["Authentication"])
+
+app.include_router(python_router, tags=["python questions"], prefix="/python_questions")
--- src/python_questions/routes.py
+++ src/python_questions/routes.py
@@ -1,12 +1,16 @@
-from fastapi import APIRouter, Request
+from pymongo import MongoClient
 from bson.json_util import loads, dumps
+from src.config import DB_HOST, DB_PORT_MONGO
 
+from fastapi import APIRouter, Request
+
+client = MongoClient(DB_HOST, int(DB_PORT_MONGO))
 
 router = APIRouter()
 
 
-@router.get("/question", response_description="List all questions")
-def list_questions(request: Request):
+@router.get("/all_questions", response_description="List of all python questions")
+async def list_python_questions(request: Request):
     questions = request.app.database["series"].find()
     final = dumps(questions)
     return loads(final)
