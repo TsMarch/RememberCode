97d329a85630447339a08a348f5fdf5f4bba7219
TsMarch <dementedcactus@mail.ru>
2024-02-19 00:01:07 +0500

    env update

--- alembic/env.py
+++ alembic/env.py
@@ -15,11 +15,11 @@ sys.path.append(os.path.join(sys.path[0], 'api'))
 # access to the values within the .ini file in use.
 config = context.config
 section = config.config_ini_section
-config.set_section_option(section, "DB_HOST", DB_HOST)
-config.set_section_option(section, "DB_NAME", DB_NAME)
-config.set_section_option(section, "DB_PASS", DB_PASS)
-config.set_section_option(section, "DB_USER", DB_USER)
-config.set_section_option(section, "DB_PORT", DB_PORT)
+config.set_section_option(section, "DB_HOST", "db")
+config.set_section_option(section, "DB_NAME", "postgres")
+config.set_section_option(section, "DB_PASS", "postgres")
+config.set_section_option(section, "DB_USER", "postgres")
+config.set_section_option(section, "DB_PORT", "5435")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
