8c1e16121bf349ee0b11b07e05929e51dadc9514
TsMarch <dementedcactus@mail.ru>
2024-06-10 02:26:39 +0500

    schemes private attr update

--- api/auth/schemas.py
+++ api/auth/schemas.py
@@ -1,6 +1,5 @@
 from uuid import UUID
 from pydantic import BaseModel, EmailStr, Field, ConfigDict
-from typing import Annotated, Optional
 
 
 class Token(BaseModel):
@@ -11,14 +10,13 @@ class Token(BaseModel):
 
 class User(BaseModel):
     model_config = ConfigDict(from_attributes=True)
-    nickname: str
+    nickname: str = Field(max_length=10)
     email: EmailStr
-    hashed_password: Annotated[str, Field(exclude=True)]
+    _hashed_password: str
     _id: UUID
 
 
 class UserAdditional(User):
-    id: UUID
     user_level: str | None = None
     is_premium: bool | None = None
     disabled: bool | None = None
